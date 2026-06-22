a561e245c1b3c65b854809be6c832bd049a68584
Victoria Flores <victoriafloresmello@gmail.com>
2026-06-22 13:54:37 -0300

    test(analise): testes de vazio

---
+++ tests/test_analise_vazio.py
@@ -0,0 +1,10 @@
+# teste do comportamento com entrada vazia
+from saiku import analise
+
+
+def test_analise_sem_dados():
+    res = analise.analisar([], [], [])
+    assert res["indicadores"] == {}
+    assert res["sinais"] == [
+        "Nenhum sinal forte de dificuldade de manutenção foi identificado na amostra."
+    ]
